cf0b4f976a96183dd88f9242a517d8d2cc8100f8
Antonio Andriella <aandriella@iri.upc.educlear>
2019-09-27 11:00:42 +0200

    working version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# robot_face_visualizer
+# robot_facial_expression
+## This repository contains an edited version of the baxter facial expression package. Facial expressions are customized to be used on a Tiago robot
---
+++ src/face_visualizer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+bridge = CvBridge()
+def xdisplay_callback(data):
+  #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+  print("callback")
+  try:
+    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    cv2.imshow('image', cv_image)
+    cv2.waitKey(1)
+  except CvBridgeError as e:
+    print(e)
+  #cv2.imwrite('/home/aandriella/pal/cognitive_game_ws/src/face_listener/color_img.jpg', cv_image)
+
+
+def face_listener():
+  rospy.init_node('listener', anonymous=True)
+  rospy.Subscriber("/robot/xdisplay", Image, xdisplay_callback)
+  rospy.spin()
+
+
+
+
+if __name__ == "__main__":
+  face_listener()
